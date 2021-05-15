ce472e378a1c3edb62b07eabd93b4491c9bbc878
ulises-n64 <ulises.ra.cal@gmail.com>
2021-05-15 17:48:44 -0500

    Trueque, respaldo, backend netsjs, app flutter y web en vuejs

---
+++ chatweb/telegrom-d9350a595ee65c36e64b3249f7b919f592ac395d/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      todos: null,
+    };
+  },
+
+};
+</script>
+
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ chatweb/telegrom-d9350a595ee65c36e64b3249f7b919f592ac395d/src/components/UserList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="chat-list">
+    <h3>Usuarios</h3>
+    <user-list-item
+      v-for="user in $store.state.user.list"
+      :key="user._id"
+      :id="user._id"
+      :name="user.name"
+    />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import UserListItem from '@/components/UserListItem.vue';
+
+export default {
+  name: 'ChatList',
+  components: {
+    UserListItem,
+  },
+  created() {
+    this.$store.dispatch('user/getUsers');
+  },
+  mounted() {
+    console.log('Hola mundo desde axios');
+    this.getUsers();
+  },
+  methods: {
+    async getUsers() {
+      console.log('Aqui va la peticion de usuarios');
+      await axios.get('http://localhost:4000/users').then((response) => {
+        console.log(response);
+      }).catch(e => console.log(`errrror:   ${e}`));
+    },
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  text-align: left;
+  padding-left: 16px;
+}
+</style>
